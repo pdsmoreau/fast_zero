1092be63faca2ae640c4231ffb6de4cd5623ea01
Peu <pdsmoreau@pop-os.localdomain>
2024-03-13 14:03:18 -0300

    Protege os enpoints PUT e DELETE com autenticação

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,7 @@ from sqlalchemy.pool import StaticPool
 from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import Base, User
+from fast_zero.security import get_password_hash
 
 # ...
 
@@ -39,9 +40,25 @@ def client(session):
 
 @pytest.fixture
 def user(session):
-    user = User(username='Teste', email='teste@test.com', password='testtest')
+    password = 'testtest'
+    user = User(
+        username='Teste',
+        email='teste@test.com',
+        password=get_password_hash(password),
+    )
     session.add(user)
     session.commit()
     session.refresh(user)
 
+    user.clean_password = 'testtest'
+
     return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        'token/',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    return response.json()['access_token']
