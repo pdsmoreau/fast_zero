9840fbe06e7d3cec78b3364c1720301305145d2d
Peu <pdsmoreau@pop-os.localdomain>
2024-03-21 07:53:27 -0300

    Dockerizando nossa aplicação e alterando os testes para serem executados no PostgreSQL

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,18 +10,15 @@ from fast_zero.app import app
 from fast_zero.database import get_session
 from fast_zero.models import Base, User
 from fast_zero.security import get_password_hash
+from fast_zero.settings import Settings
 
 # ...
 
 
 @pytest.fixture
 def session():
-    engine = create_engine(
-        'sqlite:///:memory:',
-        connect_args={'check_same_thread': False},
-        poolclass=StaticPool,
-    )
-    Session = sessionmaker(bind=engine)
+    engine = create_engine(Settings().DATABASE_URL)
+    Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     Base.metadata.create_all(engine)
     yield Session()
     Base.metadata.drop_all(engine)
@@ -42,10 +39,10 @@ def client(session):
 @pytest.fixture
 def token(client, user):
     response = client.post(
-        '/auth/token',
-        data={'username': user.email, 'password': user.clean_password},
+        "/auth/token",
+        data={"username": user.email, "password": user.clean_password},
     )
-    return response.json()['access_token']
+    return response.json()["access_token"]
 
 
 class UserFactory(factory.Factory):
@@ -53,34 +50,34 @@ class UserFactory(factory.Factory):
         model = User
 
     id = factory.Sequence(lambda n: n)
-    username = factory.LazyAttribute(lambda obj: f'test{obj.id}')
-    email = factory.LazyAttribute(lambda obj: f'{obj.username}@test.com')
-    password = factory.LazyAttribute(lambda obj: f'{obj.username}@example.com')
+    username = factory.LazyAttribute(lambda obj: f"test{obj.id}")
+    email = factory.LazyAttribute(lambda obj: f"{obj.username}@test.com")
+    password = factory.LazyAttribute(lambda obj: f"{obj.username}@example.com")
 
 
 @pytest.fixture
 def user(session):
-    password = 'testtest'
+    password = "testtest"
     user = UserFactory(password=get_password_hash(password))
 
     session.add(user)
     session.commit()
     session.refresh(user)
 
-    user.clean_password = 'testtest'
+    user.clean_password = "testtest"
 
     return user
 
 
 @pytest.fixture
 def other_user(session):
-    password = 'testtest'
+    password = "testtest"
     user = UserFactory(id=2, password=get_password_hash(password))
 
     session.add(user)
     session.commit()
     session.refresh(user)
 
-    user.clean_password = 'testtest'
+    user.clean_password = "testtest"
 
     return user
